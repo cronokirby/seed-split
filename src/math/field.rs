ca69f3c32d5fbe95e464734d433d1903241ea710
Lúcás Críostóir Meier <cronokirby@gmail.com>
2022-03-26 09:49:28 +0100

    Implement polynomial multiplication

--- src/math/field.rs
+++ src/math/field.rs
@@ -4,20 +4,30 @@ use std::ops;
 // Only implement equality for tests. This is to avoid the temptation to introduce
 // a timing leak through equality comparison.
 #[cfg_attr(test, derive(PartialEq))]
-pub struct BPoly<const N: usize> {
+struct BPoly<const N: usize> {
     data: [u64; N],
 }
 
 impl<const N: usize> BPoly<N> {
-    pub fn zero() -> Self {
+    fn zero() -> Self {
         Self { data: [0; N] }
     }
 
-    pub fn one() -> Self {
+    fn one() -> Self {
         // Not sure if there's a more elegant way to write this
         let mut data = [0; N];
         data[0] = 1;
         Self { data }
+    }
+
+    fn times_z(&mut self, start: u64) -> u64 {
+        let mut top = start;
+        for x in &mut self.data {
+            let new_top = *x >> 63;
+            *x = (*x << 1) | top;
+            top = new_top;
+        }
+        top
     }
 }
 
@@ -36,6 +46,35 @@ impl<const N: usize> ops::Add for BPoly<N> {
         let mut out = self;
         out += rhs;
         out
+    }
+}
+
+impl<const N: usize> ops::Mul for BPoly<N> {
+    type Output = (Self, Self);
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        // Algorithm 2.35 in "Guide to Elliptic Curve Cryptography"
+        let (mut out_hi, mut out_lo) = (Self::zero(), Self::zero());
+
+        for k in (0..64).rev() {
+            for j in 0..N {
+                if ((self.data[j] >> k) & 1) != 1 {
+                    continue;
+                }
+                // Hopefully all of this can get inlined
+                let mut view = Self::zero();
+                view.data[..(N - j)].copy_from_slice(&out_lo.data[j..]);
+                view.data[(N - j)..].copy_from_slice(&out_hi.data[..j]);
+                view += rhs;
+                out_lo.data[j..].copy_from_slice(&view.data[..(N - j)]);
+                out_hi.data[..j].copy_from_slice(&view.data[(N - j)..]);
+            }
+            if k != 0 {
+                let top = out_lo.times_z(0);
+                out_hi.times_z(top);
+            }
+        }
+        (out_hi, out_lo)
     }
 }
 
@@ -74,9 +113,31 @@ mod test {
         }
     }
 
+    proptest! {
+        #[test]
+        fn test_multiplication_commutative(a in arb_bpoly(), b in arb_bpoly()) {
+            assert_eq!(a * b, b * a)
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn test_mul_one_identity(a in arb_bpoly()) {
+            assert_eq!(a * BPoly::one(), (BPoly::zero(), a))
+        }
+    }
+
     #[test]
     fn test_one_plus_one_is_zero() {
         let one = BPoly::<4>::one();
         assert_eq!(one + one, BPoly::zero())
+    }
+
+    #[test]
+    fn test_z_times_z_is_z_squared() {
+        let z = BPoly::<4> { data: [2, 0, 0, 0] };
+        let z2 = BPoly::<4> { data: [4, 0, 0, 0] };
+
+        assert_eq!(z * z, (BPoly::zero(), z2))
     }
 }
